f5a60662031ff43d936c12681af4a02b851c02c2
mkasi799 <mkasi799@mtroyal.ca>
2026-08-03 10:30:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -13,17 +13,18 @@ function addLogo() {
 function renderIntro() {
     screen.innerHTML = `
         ${addLogo()}
-        <div class="intro-text">
-            <p>
-                You will participate in a brief virtual meeting with other participants. During the meeting, your self-view will be hidden to reduce distractions and standardize the participant experience.
-            </p>
+        <div class="center">
+            <div class="intro-text">
+                <p>
+                    You will participate in a brief virtual meeting with other participants. During the meeting, your self-view will be hidden to reduce distractions and standardize the participant experience.
+                </p>
 
-            <p>
-                Although you will not be able to see your own video feed, your camera will remain on throughout the interaction, and your video will be visible to the other participants. Likewise, you will be able to see other participants. Please remain connected until the meeting concludes.
-            </p>
-        </div>
+                <p>
+                    Although you will not be able to see your own video feed, your camera will remain on throughout the interaction, and your video will be visible to the other participants. Likewise, you will be able to see other participants. Please remain connected until the meeting concludes.
+                </p>
+            </div>
 
-        <button id="proceedBtn">Proceed</button>
+            <button id="proceedBtn">Proceed</button>
        
     `;
 
@@ -194,7 +195,7 @@ function startStudy() {
                 <div class="bottom-ui">
 
                     <div class="control" id="muteButton">
-                        <img src="mic.svg" class="icon" id="muteIcon">
+                        <img src="mic-muted.svg" class="icon" id="muteIcon">
                         <span>Mute</span>
                     </div>
 
